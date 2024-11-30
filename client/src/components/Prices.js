692544583bd0c5c8c2b2ceff60453461c36ba579
CesarAGS100000 <csaralonsogs@gmail.com>
2024-11-29 19:15:02 -0600

    Filtros por categoria y precio

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+    {
+      _id: 0,
+      name: "$0 a 200",
+      array: [0, 200],
+    },
+    {
+      _id: 1,
+      name: "$200 a 399",
+      array: [200, 399],
+    },
+    {
+      _id: 2,
+      name: "$400 a 599",
+      array: [400, 599],
+    },
+    {
+      _id: 3,
+      name: "$600 a 799",
+      array: [600, 799],
+    },
+    {
+      _id: 4,
+      name: "$800 a 999",
+      array: [800, 999],
+    },
+    {
+      _id: 4,
+      name: "$1000 en delante",
+      array: [1000, 9999],
+    },
+  ];
